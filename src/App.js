b148be14f41cf56457724eb00fd5819bc09ce3b2
cherubicanne <cherubicfuertes@gmail.com>
2022-07-01 12:36:19 +0800

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import Dog from './dog.gif';
+import Logo from './VetVerse-Logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
+      <img src={Dog} alt="dog.gif" />
+
 
        <h1> Building something great.</h1>
        <h5> Check back with us soon! </h5>
+
+       <p>Brought to you by: </p>
+       <img src={Logo} alt="VetVerse" />
      
       </header>
     </div>
